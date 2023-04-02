d55189f170cf7515df01da68d4179ff398cf8bae
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-04-02 11:46:16 +0200

    Add files via upload

---
+++ MidTerm2022_2/task1/Airline.java
@@ -0,0 +1,31 @@
+package zad1;
+import java.util.*;
+
+public abstract class Airline implements SpecialRequirements {
+private String name;
+private String[] fleet;
+
+public Airline(String name, String[] fleet) {
+	this.name=name;
+	this.fleet=fleet;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name=name;
+}
+public String[] getFleet() {
+	return fleet;
+}
+public void setFleet(String[] fleet) {
+	this.fleet=fleet;
+}
+
+public abstract boolean availabilityOfPromotionalPrices();
+
+@Override
+public String toString() {
+	return String.format("Airline %s has fleet: %s.", name, Arrays.toString(fleet));
+}
+}
