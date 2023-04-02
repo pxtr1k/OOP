22e12142e0b06776ff10fa6d13a2c686bad47b34
pxtr1k <124206680+pxtr1k@users.noreply.github.com>
2023-04-02 11:37:11 +0200

    Update CameraDrone.java

--- MidTerm2022_1/task1/CameraDrone.java
+++ MidTerm2022_1/task1/CameraDrone.java
@@ -1,41 +1,39 @@
-package zad1;
+public class CameraDrone extends Aircraft implements Unmanned{
+    private final int range;
+    private int[] qualities;
 
-public class CameraDrone extends Aircraft implements Unmanned {
+    public CameraDrone(String model, String dateProduced, int range, int[] qualities){
+        super(model, dateProduced);
+        this.range=range;
+        this.qualities=qualities;
+    }
+    public void setQualities(int[] qualities) {
+        this.qualities = qualities;
+    }
+    public int[] getQualities() {
+        return qualities;
+    }
+    public int getRange() {
+        return range;
+    }
+    @Override
+    public Status aircraftStatus(){
+        double sum=0.;
+        for(int quality:qualities){
+            sum+=quality;
+        }
+        double average=sum/qualities.length;
+        if(average>70 && average<=100) return Status.FUNCTIONAL;
+        if(average>=50 && average<=70) return Status.NEED_CHECK;
+        return Status.OUT_OF_SERVICE;
+    }
+    @Override
+    public double coverArea(Number height){
+        return (double)((height))*range;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s and has %d elements.", super.toString(), qualities.length);
+    }
 
-	private final int range;
-	private int[]qualities;
-	
-	public CameraDrone(String model, String dateProduced, int range, int[]qualities) {
-		super(model, dateProduced);
-		this.range=range;
-		this.qualities=qualities;
-	}
-	public int getRange() {
-		return range;
-	}
-	public int[] getQualities() {
-		return qualities;
-	}
-	public Status aircraftStatus() {
-		double sum=0;
-		for(int quality:qualities) {
-			sum+=quality;
-		}
-		double average=sum/qualities.length;
-		if(average>70 && average<=100)
-			return Status.FUNCTIONAL;
-		if(average>=50 && average<=70)
-			return Status.NEED_CHECK;
-		else
-			return Status.OUT_OF_SERVICE;
-		
-	}
-	public double coverArea(Number height) {
-		return range*(double)height;
-	}
-	@Override
-	public String toString() {
-		return String.format("%s and has %d elements", super.toString(), qualities.length);
-	}
-	
 }
